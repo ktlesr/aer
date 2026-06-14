d614deb81f10176a0b57046d7888513d16f005e3
ktlesr <kutlueser@gmail.com>
2026-06-14 09:05:59 +0300

    feat(redaction): redactJson() with 6 detectors + unit tests (TDD)

---
+++ apps/web/lib/redaction/index.ts
@@ -0,0 +1,8 @@
+export { redactJson } from "./redactJson";
+export { PATTERNS, luhnValid } from "./patterns";
+export type {
+  FindingType,
+  Severity,
+  RedactionFinding,
+  RedactionResult,
+} from "./types";
